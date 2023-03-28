23c91dd216a2cfb66d36fcca42682546a7837d51
ミューズ / Muse <91707462+MuseMyuzu@users.noreply.github.com>
2023-03-28 15:22:57 +0900

    commonを使った体制に移行完了

--- speech-to-morse/app.py
+++ speech-to-morse/app.py
@@ -40,15 +40,34 @@ import json
 import pykakasi
 import uuid
 import os
+import datetime
 
 # テンプレート、staticはFlaskWithOpenAIフォルダから
 app = Flask(__name__, static_url_path="", static_folder="../", template_folder="../")
+
+# 1日以上経過したファイルを削除
+def remove_old_files(folder_path):
+    now = datetime.datetime.now()
+    # ファイルのリストを取得
+    files = os.listdir(folder_path)
+
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path):
+            # ファイルの更新日時を取得
+            modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+            # 現在の日時と比較し、1日以上経過したファイルを削除
+            if (now - modified_time).days > 1:
+                os.remove(file_path)
+                print(f"{file}を削除しました")
 
 # 録音した音声データを保存する
 @app.route('/save_audio', methods=['POST'])
 def save_wav():
     # 録音した音声を一時的に保存するファイル名
     WEBM_FILE = './audio/recording_' + str(uuid.uuid4()) + '.webm'
+    # オーディオデータの入ったファイルのパスのみ
+    AUDIO_PATH = './audio/'
     # javascriptからファイルを受け取る
     audio_file = request.files["audio_data"]
     lang_file = request.files["lang"]
@@ -76,6 +95,8 @@ def save_wav():
     result_dict = dict(user_text=text, bot_text=morse_text)
 
     os.remove(WEBM_FILE)
+    # 1日以上経過したものは削除
+    remove_old_files(AUDIO_PATH)
     
     # jsonを返す
     return json.dumps(result_dict)
