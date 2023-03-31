8386c3468b0423184bbee6a28cbe9cb50dfc8d78
ミューズ / Muse <91707462+MuseMyuzu@users.noreply.github.com>
2023-03-31 09:39:14 +0900

    音声を重ならないようにつなげる？

--- talk-gpt/static/talkgpt/play_audio.js
+++ talk-gpt/static/talkgpt/play_audio.js
@@ -24,8 +24,9 @@ volumeControl.addEventListener('input', function() {
   }
 });
 
+/*
 // Web Audio APIを使用して音声を再生
-function playAudio(audioData) {
+function playAudio(blob) {
   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
   gainNode = audioCtx.createGain();
@@ -43,7 +44,52 @@ function playAudio(audioData) {
       audioSrc.start();
     });
   };
-  fileReader.readAsArrayBuffer(audioData);
+  fileReader.readAsArrayBuffer(blob);
+}
+*/
+
+let audioBufferList = [];
+
+function playAudio(blob) {
+  if (!audioCtx) {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    gainNode = audioCtx.createGain();
+    // 初期音量を設定
+    updateVolume();
+  }
+
+  const fileReader = new FileReader();
+  fileReader.onload = () => {
+    const arrayBuffer = fileReader.result;
+    audioCtx.decodeAudioData(arrayBuffer, (decodedData) => {
+      audioBufferList.push(decodedData);
+      if (audioBufferList.length === 1) {
+        playBuffer();
+      }
+    });
+  };
+  fileReader.readAsArrayBuffer(blob);
+}
+
+function playBuffer() {
+  const audioSrc = audioCtx.createBufferSource();
+  const buffer = audioCtx.createBuffer(audioBufferList.length, audioBufferList[0].length, audioBufferList[0].sampleRate);
+
+  for (let i = 0; i < audioBufferList.length; i++) {
+    buffer.getChannelData(i).set(audioBufferList[i].getChannelData(0));
+  }
+
+  audioSrc.buffer = buffer;
+  audioSrc.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  audioSrc.start();
+
+  audioSrc.onended = () => {
+    audioBufferList.shift();
+    if (audioBufferList.length > 0) {
+      playBuffer();
+    }
+  };
 }
 
 function updateVolume(){
